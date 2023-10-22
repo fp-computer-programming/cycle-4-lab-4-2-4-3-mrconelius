cae4392f6f196127d707af841162d375c82a9ba2
mrconelius <144148053+mrconelius@users.noreply.github.com>
2023-10-22 18:05:21 -0400

    Add files via upload

---
+++ lab_4-2.py
@@ -0,0 +1,6 @@
+school = "Fairfield Prep"
+first_half, second_half = school.split("Fairfield Prep")
+print("First Half:Fairfield", first_half)
+print("Second Half:Prep", second_half)
+full_name = first_half + second_half
+print("Full Name:Fairfield Prep", full_name)
---
+++ lab_4-3.py
@@ -0,0 +1,8 @@
+string1 = input("fairfield")
+string2 = input ("prep")
+if string1 == string2:
+    print("Both strings are equal.")
+elif string1 > string2:
+    print("The first string is greater than the second string.")
+else:
+    print("The first string is less than the second string.")
